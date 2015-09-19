1e141b38decd7150be26e7d91b023540091ce274
Ronnel Davis <ronnel_davis@yahoo.com>
2015-09-20 00:21:33 +0530

    Add new page

--- js/record.js
+++ js/record.js
@@ -66,6 +66,10 @@ function startRecord(){
     timer = window.setInterval(function() {
         currentTime += 1;
         document.getElementById("current-time").innerHTML = currentTime;
+        scale(document.getElementById("timer-inner"), currentTime/15);
+        if(currentTime==15){
+            endRecord();
+        }
     }, 1000);
     if(mediaRecorder){
         mediaRecorder.start();
@@ -145,6 +149,56 @@ function visualize(stream) {
     canvasCtx.stroke();
 
   }
+}
+
+
+
+var fromHidden = -90;
+
+// utility funciton to align 0 degrees with top
+// takes degrees and returns degrees - 45
+function topAlign(degrees) {
+    return degrees - 45
+};
+
+// utility function to rotate a jQuery element
+// takes element and the degree of rotation (from the top)
+function rotate(el, degrees) {
+	var degrees = topAlign(degrees || 0);
+	el.css(
+		'transform', 'rotate('+degrees+'deg)',
+		'-webkit-transform', 'rotate('+degrees+'deg)',
+		'-moz-transform', 'rotate('+degrees+'deg)',
+		'-ms-transform', 'rotate('+degrees+'deg)',
+		'-o-transform', 'rotate('+degrees+'deg)'
+	)
+}
+
+// function to draw semi-circle
+// takes a jQuery element and a value (between 0 and 1)
+// element must contain four .arc_q elements
+function circle(el, normalisedValue) {
+	var degrees = normalisedValue * 360;             // turn normalised value into degrees
+	var counter = 1;                                 // keeps track of which quarter we're working with
+	el.find('.arc_q').each(function(){               // loop over quarters..
+		var angle = Math.min(counter * 90, degrees); // limit angle to maximum allowed for this quarter
+		rotate($(this), fromHidden + angle);         // rotate from the hiding place
+		counter++; // track which quarter we'll be working with in next pass over loop
+	});
+	if (degrees > 90) {                              // hide the cover-up square soon as we can
+		el.find('.arc_cover').css('display', 'none');
+	}
+}
+
+// uses the the circle function to 'animate' drawing of the semi-circle
+// incrementally increses the value passed by 0.01 up to the value required
+function animate(el, normalisedValue, current) {
+	var current = current || 0;
+	circle(el, current);
+	if (current < normalisedValue) {
+		current += 0.01;
+		setTimeout(function () { animate(el, normalisedValue, current); }, 1);
+	}
 }
 
 

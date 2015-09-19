286c1cf39900c61fd74affcc06aad199140196b0
Ashiq Muhammed <mbgashiq@gmail.com>
2015-09-19 22:03:58 +0530

    Submit button made responsive

--- js/profile.js
+++ js/profile.js
@@ -5,4 +5,25 @@ $(document).ready(function(){
   $("input").blur(function(){
       $(".footer").fadeIn(0);
   });
+
+  $("#submit").click(submit);
 });
+
+function submit(){
+		var obj=[
+			document.getElementById("name").value,
+			document.getElementById("email").value,
+      document.getElementById("phone").value,
+      document.getElementById("school").value,
+      document.getElementById("prfname").value,
+      document.getElementById("status").value,
+		]
+        if (!document.getElementById("name").value || !document.getElementById("email").value || !document.getElementById("phone").value || !document.getElementById("school").value || !document.getElementById("prfname").value || !document.getElementById("status").value) {
+            alert("Fill All Input Fields Ans Try Again");
+            return;
+        }
+        else {
+          alert("Changes Have Been Saved");
+          return;
+        }
+}

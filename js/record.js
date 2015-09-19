8ca47e16cdf0fd52fa9375e87d682192c32ae816
Ronnel Davis <ronnel_davis@yahoo.com>
2015-09-19 19:14:50 +0530

    Recorder complete

--- js/record.js
+++ js/record.js
@@ -1,4 +1,4 @@
-
+window.mediaRecorder = false;
 
 window.onload = function(){
 
@@ -10,8 +10,8 @@ window.onload = function(){
         onSpringUpdate: function(spring) {
             var val = mapValueFromRangeToRange( spring.getCurrentValue() , 0, -1, 1, 0.5);
             scale(this.el, val);
-          }
-      });
+        }
+    });
 
     photo.addEventListener(downEvt, function() {
         spring.setEndValue(-1);
@@ -23,13 +23,33 @@ window.onload = function(){
         endRecord();
     });
 
+    if (navigator.getUserMedia) {
+        console.log('getUserMedia supported.');
+        navigator.getUserMedia ({
+            // constraints - only audio needed for this app
+            audio: true,
+            video: false
+        },
+        // Success callback
+        function(stream) {
+            mediaRecorder = new MediaRecorder(stream);
+        },
+
+        // Error callback
+        function(err) {
+            console.log('The following getUserMedia error occured: ' + err);
+        });
+    } else {
+        console.log('getUserMedia not supported on your browser!');
+    }
+
 }
 
 window.scale = function scale(el, val) {
-  el.style.mozTransform =
-  el.style.msTransform =
-  el.style.webkitTransform =
-  el.style.transform = 'scale3d(' + val + ', ' + val + ', 1)';
+    el.style.mozTransform =
+    el.style.msTransform =
+    el.style.webkitTransform =
+    el.style.transform = 'scale3d(' + val + ', ' + val + ', 1)';
 }
 
 window.timer = {};
@@ -39,9 +59,34 @@ function startRecord(){
         currentTime += 1;
         document.getElementById("current-time").innerHTML = currentTime;
     }, 1000);
+
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
 }
 
 function endRecord(){
     window.clearInterval(timer);
     currentTime = 0;
+    if(mediaRecorder){
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+
+        mediaRecorder.ondataavailable = function(e) {
+            console.log("data available");
+            var audio = document.createElement('audio');
+            audio.setAttribute('controls', '');
+            document.body.appendChild(audio);
+            var audioURL = window.URL.createObjectURL(e.data);
+            audio.src = audioURL;
+            audio.play();
+        }
+    }
 }
+
+
+navigator.getUserMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);

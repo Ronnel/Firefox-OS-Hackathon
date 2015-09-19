1fb741dd29eb242f85d1d17c500732245aed0a82
Vrushabh <vrjgamer@gmail.com>
2015-09-20 00:58:19 +0530

    ron over to you

---
+++ js/front_page.js
@@ -0,0 +1,10 @@
+var contact = function(id, name, dp){
+  this.id=id;
+  this.name:test;
+  this.dp:dp;
+  var div= document.createElement("div");
+  div.setAttribute("class", "contacts");
+  div.innerHTML="<div class='profile_pic' style='background: url("+dp+"); background-size: cover; -webkit-background-size: cover; -moz-background-size: cover;'></div><div class='name'>"+name+"</div><div class='addButton'></div>";
+  this.div=div;
+  return this;
+}
